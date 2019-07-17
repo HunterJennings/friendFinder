86cb2482f089a5153911eea48d04a0ec0d9691db
Hunter Jennings <hunterjennings@hotmail.com>
2019-07-16 22:53:06 -0500

    did what i could need help with it

---
+++ app/data/friends.js
@@ -0,0 +1,93 @@
+var friends = [
+    {
+        name:"Graves",
+        photo: "https://www.mobafire.com/images/avatars/graves-classic.png",
+        scores: 
+        [
+            "2",
+            "2",
+            "3",
+            "1",
+            "5",
+            "3",
+            "5",
+            "1",
+            "2",
+            "4"
+
+        ]
+    },
+    {
+        name:"Nidalee",
+        photo: "https://statici.behindthevoiceactors.com/behindthevoiceactors/_img/chars/nidalee-league-of-legends-3.26.jpg",
+        scores: 
+        [
+            "1",
+            "3",
+            "4",
+            "4",
+            "2",
+            "5",
+            "5",
+            "3",
+            "2",
+            "1"
+
+        ]
+    },
+    {
+        name:"Lee Sin",
+        photo: "https://lolskinshop.com/wp-content/uploads/2015/04/LeeSin_5.jpg",
+        scores: 
+        [
+            "5",
+            "4",
+            "3",
+            "2",
+            "1",
+            "1",
+            "2",
+            "3",
+            "5",
+            "4"
+
+        ]
+    },
+    {
+        name:"Ahri",
+        photo: "https://www.mobafire.com/images/avatars/ahri-classic.png",
+        scores: 
+        [
+            "4",
+            "2",
+            "3",
+            "5",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "5"
+
+        ]
+    },
+    {
+        name:"Lux",
+        photo: "https://www.mobafire.com/images/avatars/lux-elementalist.png",
+        scores: 
+        [
+            "3",
+            "3",
+            "4",
+            "1",
+            "2",
+            "2",
+            "5",
+            "1",
+            "3",
+            "5"
+
+        ]
+    }
+];
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,34 @@
+var friends = require("../data/friends.js");
+module.exports = function(app) {
+    app.get("/api/friends", function(req, res) {
+      res.json(friends);
+    });
+    app.post("api/friends", function(req, res){
+        var match = {
+            name: "",
+            photo:"",
+            difference: 1000
+        };
+        console.log(req.body);
+        var userData = req.body;
+        var userScores = userData.scores;
+        console.log(userScores);
+        
+        var totalDifference =0;
+
+        for (var i = 0; i <friends.length; i++){
+            console.log (friends [i]);
+            totalDifference=0;
+            for (var x =0; x<friends[i].scores[x]; x++){
+                totalDifference += Math.abs(parseInt(userScores[x]) - parseInt(friends[i].scores[x]));
+                if (totalDifference<= match.difference){
+                    match.name = friends[i].name;
+                    match.photo = friends[i].photo;
+                    match.difference = totalDifference;
+                }
+            }
+        }
+        friends.push(userData);
+        res.json(match);
+    });
+}
