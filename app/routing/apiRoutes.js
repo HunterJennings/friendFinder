3231aebb1cd6ddcf998a2dd2081a08b126643fc2
Hunter Jennings <hunterjennings@hotmail.com>
2019-07-17 16:49:48 -0500

    finished the Hw

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,7 +3,7 @@ module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
       res.json(friends);
     });
-    app.post("api/friends", function(req, res){
+    app.post("/api/friends", function(req, res){
         var match = {
             name: "",
             photo:"",
@@ -14,19 +14,25 @@ module.exports = function(app) {
         var userScores = userData.scores;
         console.log(userScores);
         
-        var totalDifference =0;
+        // var totalDifference =0;
 
         for (var i = 0; i <friends.length; i++){
             console.log (friends [i]);
-            totalDifference=0;
-            for (var x =0; x<friends[i].scores[x]; x++){
+           var totalDifference=0;
+            for (var x =0; x<friends[i].scores; x++){
                 totalDifference += Math.abs(parseInt(userScores[x]) - parseInt(friends[i].scores[x]));
+                console.log(totalDifference);
+                // if (totalDifference<= match.difference){
+                //     match.name = friends[i].name;
+                //     match.photo = friends[i].photo;
+                //     match.difference = totalDifference;
+                // }
+            }
                 if (totalDifference<= match.difference){
                     match.name = friends[i].name;
                     match.photo = friends[i].photo;
                     match.difference = totalDifference;
                 }
-            }
         }
         friends.push(userData);
         res.json(match);
